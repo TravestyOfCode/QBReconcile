c4525b43fb699db45d9e46ce85e7753cef0d9140
travis@travestyofcode.com <travis@travestyofcode.com>
2025-04-12 22:55:55 -0400

    Adds IRequest and IRequestHandler interfaces.

---
+++ QBReconcile/Services/IRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace QBReconcile.Services;
+
+public interface IRequest<TResult>
+{
+
+}
+
+public interface IRequestHandler<TRequest, TResult> where TRequest : IRequest<TResult>
+{
+    public Task<TResult> Handle(TRequest request, CancellationToken cancellationToken = default);
+}
