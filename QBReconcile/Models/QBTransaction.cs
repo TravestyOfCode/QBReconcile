adc5cdf9f4914c53914fcde4e05e768e53704dd8
travis@travestyofcode.com <travis@travestyofcode.com>
2025-04-13 07:48:03 -0400

    Adds QBTransaction model.

---
+++ QBReconcile/Models/QBTransaction.cs
@@ -0,0 +1,11 @@
+﻿namespace QBReconcile.Models;
+
+public class QBTransaction
+{
+    public string? Type { get; set; }
+    public DateTime? Date { get; set; }
+    public string? Num { get; set; }
+    public string? Name { get; set; }
+    public bool? Clr { get; set; }
+    public decimal? Amount { get; set; }
+}
