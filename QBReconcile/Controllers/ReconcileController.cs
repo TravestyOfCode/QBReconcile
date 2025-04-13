6c845a6af54aaac3013ca31231507e49da11f82c
travis@travestyofcode.com <travis@travestyofcode.com>
2025-04-13 07:49:24 -0400

    Adds action to get unreconciled transactions by account with view.

--- QBReconcile/Controllers/ReconcileController.cs
+++ QBReconcile/Controllers/ReconcileController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using QBReconcile.Models;
 using QBReconcile.Services;
 using QBReconcile.Services.Reconcile;
 
@@ -16,6 +17,20 @@ public class ReconcileController(IServiceProvider services) : Controller
         if (result.WasSuccessful)
         {
             return View(result.Value);
+        }
+
+        return StatusCode(result.StatusCode);
+    }
+
+    public async Task<IActionResult> Process(GetUnclearedTransactionsByAccount request, CancellationToken cancellationToken)
+    {
+        var handler = services.GetRequiredService<IRequestHandler<GetUnclearedTransactionsByAccount, Result<List<QBTransaction>>>>();
+
+        var result = await handler.Handle(request, cancellationToken);
+
+        if (result.WasSuccessful)
+        {
+            return PartialView(result.Value);
         }
 
         return StatusCode(result.StatusCode);
