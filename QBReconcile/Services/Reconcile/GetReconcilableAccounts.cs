07cd5e2641a8cba893b44df72c6115bc8e958eaa
travis@travestyofcode.com <travis@travestyofcode.com>
2025-04-13 06:38:39 -0400

    Adds query to get reconcilable accounts and register handler in DI container.

---
+++ QBReconcile/Services/Reconcile/GetReconcilableAccounts.cs
@@ -0,0 +1,50 @@
+﻿using System.Xml.Linq;
+
+namespace QBReconcile.Services.Reconcile;
+
+public class GetReconcilableAccounts : IRequest<Result<List<string>>>
+{
+
+}
+
+public class GetReconcilableAccountsHandler(QBConnection conn, ILogger<GetReconcilableAccountsHandler> logger)
+    : IRequestHandler<GetReconcilableAccounts, Result<List<string>>>
+{
+    public async Task<Result<List<string>>> Handle(GetReconcilableAccounts request, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var response = await Task.Run(() => conn.ProcessRequest(Resource.ReconcilableAccountQuery));
+
+            if (response != null)
+            {
+                return ParseResponse(response);
+            }
+
+            return Error.ServerError();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Unexpected error handling request: {request}", request);
+
+            return Error.ServerError();
+        }
+    }
+
+    private List<string> ParseResponse(string response)
+    {
+        var accounts = new List<string>();
+
+        var doc = XDocument.Parse(response);
+        foreach (var element in doc.Descendants("AccountRet"))
+        {
+            var name = element.Element("FullName")?.Value;
+
+            if (!name.IsNullOrEmpty())
+            {
+                accounts.Add(name!);
+            }
+        }
+        return accounts;
+    }
+}
---
+++ QBReconcile/Services/ServiceBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using QBReconcile.Services.Reconcile;
+
+namespace QBReconcile.Services;
+
+public static class ServiceBuilder
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddTransient<IRequestHandler<GetReconcilableAccounts, Result<List<string>>>>(s => new GetReconcilableAccountsHandler(s.GetRequiredService<QBConnection>(), s.GetRequiredService<ILogger<GetReconcilableAccountsHandler>>()));
+
+        return services;
+    }
+}
