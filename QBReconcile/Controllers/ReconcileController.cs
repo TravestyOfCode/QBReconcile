e99716732c81d36b6d1bc0c570979763be23bb49
travis@travestyofcode.com <travis@travestyofcode.com>
2025-04-13 06:39:52 -0400

    Adds reconcile controller and index view.

---
+++ QBReconcile/Controllers/ReconcileController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using QBReconcile.Services;
+using QBReconcile.Services.Reconcile;
+
+namespace QBReconcile.Controllers;
+
+public class ReconcileController(IServiceProvider services) : Controller
+{
+    public async Task<IActionResult> Index(GetReconcilableAccounts request, CancellationToken cancellationToken)
+    {
+        var handler = services.GetRequiredService<IRequestHandler<GetReconcilableAccounts, Result<List<string>>>>();
+
+        var result = await handler.Handle(request, cancellationToken);
+
+        if (result.WasSuccessful)
+        {
+            return View(result.Value);
+        }
+
+        return StatusCode(result.StatusCode);
+    }
+}
